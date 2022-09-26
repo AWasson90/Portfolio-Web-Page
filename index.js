a1449bb7c3830de760a927ccdc113f25d9f744ba
Adam Wasson <114330429+AWasson90@users.noreply.github.com>
2022-09-26 02:54:21 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewpoint" content="width=device-width">
+    <title>adam-wasson-website</title>
+    <link href="style.css" rel="stylesheet"
+      type="text/css" />
+    <link href="http://cdnjs.cloudfare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+      rel="stylesheet"
+      type="text/css" />
+  </head>
+  <body>
+    <script src="script.js"></script>
+  </body>
+</html>
